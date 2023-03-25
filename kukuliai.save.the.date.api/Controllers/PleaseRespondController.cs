61a94fbfbaee4e9154963051d7ed199ee74d765a
lightTAKE <tadas.kekys@gmail.com>
2023-03-25 13:04:04 +0200

    rsvp controller init

---
+++ kukuliai.save.the.date.api/Controllers/PleaseRespondController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kukuliai.save.the.date.api.Controllers;
+
+[ApiController]
+[Route("rsvp/")]
+public class PleaseRespondController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "hello";
+    }
+}
